eefb28063f3b00065af91d3b172debe0656f4274
RyanEggert <Ryan.Eggert@students.olin.edu>
2014-04-04 12:57:48 -0400

    Pass in tosearch variable name

--- DiscourseAnalysisCounter.py
+++ DiscourseAnalysisCounter.py
@@ -1,10 +1,13 @@
 # DiscourseAnalysisCounter.py
 import re  # Import regular expressions module
 
+#  Be sure to replace apostrophe character with ' in all text files before proceeding.
+
+
 ##################FUNCTION-DEFINITION##################
 
 
-def Search_name_for(TeamNumber, PersonName, SearchTerm):
+def Search_name_for(TeamNumber, PersonName, SearchTerm, DataToSearch):
     """Finds a text transcript of a specific team's member. Searches for the regex SearchTerm"""
     pname = PersonName.lower()
      # Ensure all characters in name are lowercase (naming
@@ -13,12 +16,12 @@ def Search_name_for(TeamNumber, PersonName, SearchTerm):
 
     # Assemble the textfile filepath
     
-    FileName = r'/home/reggert/Dropbox/Discourse Files 2013-2014/Analysis/Team ' + \
-        str(TeamNumber) + '/SinglePersonTexts/' + str(procPersonName) + '.txt'
+    # FileName = r'/home/reggert/Dropbox/Discourse Files 2013-2014/Analysis/Team ' + \
+        # str(TeamNumber) + '/SinglePersonTexts/' + str(procPersonName) + '.txt'
 
 
-    # FileName = r'C:/Users/reggert/Dropbox/Discourse Files 2013-2014/Analysis/Team ' + \
-    #     str(TeamNumber) + '/SinglePersonTexts/' + str(procPersonName) + '.txt'
+    FileName = r'C:/Users/reggert/Dropbox/Discourse Files 2013-2014/Analysis/Team ' + \
+         str(TeamNumber) + '/SinglePersonTexts/' + str(procPersonName) + '.txt'
 
 
         #Change first part of filepath to reflect user's dropbox location
@@ -32,7 +35,7 @@ def Search_name_for(TeamNumber, PersonName, SearchTerm):
     nopuncdata = data.translate(None, ";',.? !")  # Deletes the characters enclosed in double quotes from data and saves as nopuncdata
     nopunclinedata = nopuncdata.replace('\r', '').replace('\n','')  # Remove carriage returns and newline characters
 
-    return len(re.findall(SearchTerm, data))  # Switch nopuncdata with data or nopunclinedata
+    return len(re.findall(SearchTerm, eval(DataToSearch)))  # return total number of found instances of SearchTerm
 
     # for matchstr in re.findall(SearchTerm, nopuncdata):
     #     print matchstr
@@ -41,10 +44,12 @@ def Search_name_for(TeamNumber, PersonName, SearchTerm):
 #########################BEGIN#########################
 
 
-toFind = '(?i) we | we\'' # Regular Expression input. (?i) is case insensitive
+toFind = '(?i)/like/|/um/|/ah/|/well/|/so/' # Regular Expression input. (?i) is case insensitive
 
-TeamMembers = ['Alyssa Little', 'Diana Pitkaranta', 'Lawrence Lee', 'Tom Dominski', 'Trevor Able']  # List of team members. Change to reflect current team
+TeamMembers = ['Eric Abramson', 'John Powers', 'Ruth Thompson', 'Mary Gothel', 'Mark Castellani']  # List of team members. Change to reflect current team
 
 for x in range(len(TeamMembers)):
-    Count = Search_name_for(3, TeamMembers[x], toFind)
-    print TeamMembers[x] + '   ' + str(Count)
+    
+    Count = Search_name_for(1, TeamMembers[x], toFind, 'nopunclinedata')  # Choose data, nopuncdata, or nopunclinedata
+    
+    print TeamMembers[x] + '|' + str(Count)  # Output counted and labeled data for copying directly into MS Excel
